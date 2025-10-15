a9409ffa3bebf4f3b46d5da8e25f8ba94db54f1e
Samir Aoulad Amar <aouladamarsamirr2@gmail.com>
2025-10-15 23:02:00 +0100

    update(structure):relpace backend laravel

--- B_Application/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\homeController;
-
- 
-
-Route::get('/', [homeController::class, 'index'])->name('home');
-Route::get('/products', [homeController::class, 'index'])->name('home');
