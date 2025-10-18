a9cf6ebace0852b22a6a7098ca6f3b28b6b49bae
Samir Aoulad Amar <aouladamarsamirr2@gmail.com>
2025-10-18 13:11:49 +0100

    update(UI):navbar bergermune develope design to better

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -51,7 +51,6 @@ export default function Navbar() {
               <>
                 <div className="divhamburger">
                   <input id="burger-toggle" type="checkbox" className="burger-toggle" />
-
                   <label className="hamburger" htmlFor="burger-toggle">
                     <svg viewBox="0 0 32 32">
                       <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
